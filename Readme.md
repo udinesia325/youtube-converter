131af3fed2556b3b8938c3536ddd1d5b0adb1817
udinesia325 <udinesia325@gmail.com>
2022-01-21 13:20:47 +0700

    fist commit

---
+++ Readme.md
@@ -0,0 +1,19 @@
+# Youtube Downloader
+
+### instalasi
+- clone / download repo ini 
+- masuk ke folder yang sudah di download
+- lalu ketikkan ini
+```sh
+$ npm install
+$ npm start
+```
+- lalu buka di browser localhost:3000
+- paste alamat video youtube yang anda inginkan
+
+
+#### note
+- jika ada confirmasi di browser mengenai tipe file setuju aja :)
+
+
+build with ♥
---
+++ index.js
@@ -0,0 +1,30 @@
+const ytdl = require("ytdl-core")
+const express = require("express")
+const cors = require("cors")
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static("public"))
+
+
+
+app.post("/download", async (req, res) => {
+    try {
+        const { url } = req.body
+        const { videoDetails: { title } } = await ytdl.getInfo(url, { format: "mp3" })
+        res.setHeader("Content-Disposition", `attachment; filename="${title}.mp3"`)
+
+        ytdl(url, { format: "mp3" }).pipe(res)
+
+
+
+    } catch (error) {
+        res.redirect("/")
+    }
+
+})
+
+app.listen(PORT, () => console.log(`Server berjalan pada port ${PORT}`))
